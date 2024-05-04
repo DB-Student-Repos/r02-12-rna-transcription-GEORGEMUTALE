d4ffe83001c638717a1d3b7a2153ac9d95996ade
GEORGEMUTALE <mutalegeorge367@gmail.com>
2024-05-04 19:34:12 +0200

    DNA to RNA

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,52 @@
 #[derive(Debug, PartialEq, Eq)]
-pub struct Dna;
+pub struct Dna(String);
 
 #[derive(Debug, PartialEq, Eq)]
-pub struct Rna;
+pub struct Rna(String);
 
 impl Dna {
     pub fn new(dna: &str) -> Result<Dna, usize> {
-        unimplemented!("Construct new Dna from '{dna}' string. If string contains invalid nucleotides return index of first invalid nucleotide");
+        let invalid_index = dna.chars().position(|c| match c {
+            'G' | 'C' | 'T' | 'A' => false,
+            _ => true,
+        });
+        
+        match invalid_index {
+            Some(idx) => Err(idx),
+            None => Ok(Dna(dna.to_string())),
+        }
     }
 
     pub fn into_rna(self) -> Rna {
-        unimplemented!("Transform Dna {self:?} into corresponding Rna");
+        let rna_string = self.0.chars().map(|c| match c {
+            'G' => 'C',
+            'C' => 'G',
+            'T' => 'A',
+            'A' => 'U',
+            _ => c,
+        }).collect();
+        Rna(rna_string)
+    }
+
+    pub fn sequence(&self) -> &str {
+        &self.0
     }
 }
 
 impl Rna {
     pub fn new(rna: &str) -> Result<Rna, usize> {
-        unimplemented!("Construct new Rna from '{rna}' string. If string contains invalid nucleotides return index of first invalid nucleotide");
+        let invalid_index = rna.chars().position(|c| match c {
+            'G' | 'C' | 'A' | 'U' => false,
+            _ => true,
+        });
+        
+        match invalid_index {
+            Some(idx) => Err(idx),
+            None => Ok(Rna(rna.to_string())),
+        }
+    }
+
+    pub fn sequence(&self) -> &str {
+        &self.0
     }
 }
